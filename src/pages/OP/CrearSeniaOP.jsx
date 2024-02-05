cde769db436d3ffa23cc050899386cc927904a4e
Raul <castillosteven09@outlook.es>
2024-02-05 14:09:39 -0500

    media pipe

--- src/pages/OP/CrearSeniaOP.jsx
+++ src/pages/OP/CrearSeniaOP.jsx
@@ -31,7 +31,8 @@ import Webcam from "react-webcam";
 import { useEffect, useState, useRef } from "react";
 export default function CrearSeniaOP({ id_seccion }) {
   const webcamRef = useRef(null);
-
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
   const [concepto, setConcepto] = useState({
     definiciones: [],
     links_youtube: [],
@@ -41,7 +42,9 @@ export default function CrearSeniaOP({ id_seccion }) {
   //Conceptos
   useEffect(() => {
     ObtenerPreguntas();
+    Simon();
   }, []);
+  const [socket, setsocket] = useState(null);
 
   // Función para obtener los datos de manera asíncrona
   const ObtenerPreguntas = async () => {
@@ -74,9 +77,224 @@ export default function CrearSeniaOP({ id_seccion }) {
     //enviar a la API POR EJEMPLO xd
     console.log(imageSrc);
   };
+  /** PARA LA WEBCAM */
+  const [videoElement, setVideoElement] = useState(); //
+  //Funcion para obtener los resultados del procesamiento de mediapipe
+  const [canvasIsReady, setCanvasIsReady] = useState(false);
+  const [canvasHaveLandmarks, setcanvasHaveLandmarks] = useState(false);
+  const [canvasCtx1, setcanvasCtx1] = useState(null);
+  const [canvasElement1, setcanvasElement1] = useState(null);
+  const Simon = () => {
+    const videoElement = document.getElementsByClassName("input_video")[0];
+    const canvasElement = document.getElementsByClassName("output_canvas")[0];
+    const canvasCtx = canvasElement.getContext("2d");
+    setcanvasCtx1(canvasCtx);
+    setcanvasElement1(canvasElement);
+    //let canvasIsReady = false;
+    //let canvasHaveLandmarks = false;
+    //FUEnviarSocker();
+    function setCanvaMediaVideoSize() {
+      //Obtener las dimensiones (ancho y alto) del video
+      let videoWidth = videoElement.videoWidth;
+      let videoHeight = videoElement.videoHeight;
+
+      //Calcular la proporción entre ancho y alto para mantener el aspecto
+      let aspectRatio = videoWidth / videoHeight;
+
+      //Ajustar las dimensiones del canvas para mantener el aspecto del video
+      let canvasWidth = canvasElement.offsetWidth;
+      let canvasHeight = canvasWidth / aspectRatio;
+
+      //En caos de que el tamanio no se acople, utilizar el aspecto calculado
+      //para evitar malos redimensionamientos
+      if (canvasHeight > canvasElement.offsetHeight) {
+        canvasHeight = canvasElement.offsetHeight;
+        canvasWidth = canvasHeight * aspectRatio;
+      }
+
+      //Redimensionando el canvas para que coincida con las dimensiones calculadas
+      canvasElement.width = canvasWidth;
+      canvasElement.height = canvasHeight;
+    }
+    function onResults(results) {
+      setCanvaMediaVideoSize();
+
+      //Redimensionar el canva al video captado en por la cámara
+      setCanvaMediaVideoSize();
+
+      //Limpiar el canvas
+      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+      //canvasElement.style.border = "1px solid red";
+
+      //Llenar el canvas con el color de fondo negro y el tamanio total al que corresponde
+      canvasCtx.fillStyle = "#FFFFFF";
+
+      //Si se quiere mantener el mismo video de fondo, descomentar la linea con drawImage()
+      canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+      //canvasCtx.drawImage(videoElement, 0, 0);
+
+      //Factores para auto ajuste de tamanio
+      const baseAutosizeFactor = 5;
+      const movementAutosizeFactor = 5;
+      // var canvasHaveLandmarks;
+      //setcanvasHaveLandmarks(false);
+      if (results.multiHandLandmarks) {
+        if (results.multiHandLandmarks.length > 0) setcanvasHaveLandmarks(true);
+
+        for (const landmarks of results.multiHandLandmarks) {
+          //Obtener la coordenada z (profundidad) de todos los puntos
+          let zCoordinates = landmarks.map((landmark) => Math.abs(landmark.z));
+
+          //Obtener la minima coordenada z
+          let minZ = Math.min(...zCoordinates);
+          //let maxZ = Math.max(...zCoordinates);
+
+          //Normalizar el valor minimo de z a un valor positio o negativo
+          let movementDirection = minZ < 0 ? -1 : 1;
+
+          //Recalcular el autoajuste utilizando los factores
+          let lineWidth =
+            baseAutosizeFactor + movementDirection * movementAutosizeFactor;
+
+          //Dibujar las lineas y puntos por la captacion de la mano
+          drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+            color: "#000000",
+            lineWidth: lineWidth,
+          });
+
+          //Si se requiere dibujar los puntos, modificar la division del radio
+          drawLandmarks(canvasCtx, landmarks, {
+            color: "#000000",
+            lineWidth: lineWidth / 2,
+            radius: lineWidth / 4,
+          });
+        }
+      }
+
+      setCanvasIsReady(true);
+    }
+
+    //Cargar la libreria para usar mediapipe
+    const hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
+
+    //Establecer los parametros utilizados para la configuracion de mediapipe
+    hands.setOptions({
+      maxNumHands: 2, //Número máximo de manos aceptables o que se captaran
+      modelComplexity: 1, //1 prioriza la captación antes que la velocidad, menor valor prioriza velocidad
+      minDetectionConfidence: 0.6, //Menor valor prioriza detección menos optima, mayor valor prioriza la detección
+      minTrackingConfidence: 0.6, //Menor valor prioriza el seguimiento menos optimo, mayor valor prioriza el seguimiento correcto
+    });
+
+    //Definir el metodo de los resultados que equivale a la captacion del video
+    hands.onResults(onResults);
+
+    //Definir la captacion de video de la camara y empezarla
+    const camera = new Camera(videoElement, {
+      onFrame: async () => {
+        await hands.send({ image: videoElement });
+      },
+      width: 1280,
+      height: 720,
+    });
+    camera.start();
+
+    //Adaptar el canvas cuando cargue la camara
+    videoElement.addEventListener("loadedmetadata", () => {
+      const videoDimensions = videoElement.getBoundingClientRect();
+      canvasElement.style.width = videoDimensions.width + "px";
+      canvasElement.style.height = videoDimensions.height + "px";
+    });
+  };
+  const [EnviarSocket, setEnviarSocket] = useState(false);
+  const FUEnviarSocker = (value) => {
+    //Cuando el socket este disponible o abierto se procede a enviar las imagenes
+    socket.binaryType = "arraybuffer";
+    if (value === false) {
+      socket.close();
+      alert("Cerrando");
+      return null;
+    }
+    if (socket) {
+      socket.onopen = function (event) {
+        console.log("Connected to the web socket.");
+        alert("Abriendo");
+        //Cantidad de frames por segndo a enviar
+        const FPS = 1;
+        //Crear un ciclo encargado de enviar los frames al servidor por el web socket
+        if (value) {
+          setInterval(() => {
+            if (canvasCtx1 && canvasIsReady && canvasHaveLandmarks) {
+              setcanvasHaveLandmarks(false);
+
+              //Transformar el contenido dle canva a una URL
+              var dataURL = canvasElement1.toDataURL();
+
+              //Crear una imagen y asignarle el buffer de la imagen
+              var img = new Image();
+              img.src = dataURL;
+
+              //Cuando la imagen este lista
+              img.onload = function () {
+                //Redimensionar el lienzo (canva)
+                var canvas = document.createElement("canvas");
+                var ctx = canvas.getContext("2d");
+
+                // Redimensionar la imagen a 512x512
+                canvas.width = 512;
+                canvas.height = 512;
+                ctx.drawImage(img, 0, 0, 512, 512);
+
+                //Transformar el canva a su data para obtener el buffer de bytes
+                var dataURL = canvas.toDataURL();
+                fetch(dataURL)
+                  .then((res) => res.blob())
+                  .then((blob) => {
+                    blob.arrayBuffer().then(function (buffer) {
+                      //Enviar el buffer por el web socket
+                      console.log(buffer);
+                      socket.send(buffer);
+                    });
+                  });
+              };
+            }
+          }, 1000 / FPS);
+        }
+      };
+    }
+  };
+  //Enivar = true
+  const CambiarEnviar = async (value) => {
+    setEnviarSocket(value);
+    if (value) {
+      const socket1 = new WebSocket("ws://26.48.208.162:8080/socket");
+      alert("Aparece");
+      setsocket(socket1).then(FUEnviarSocker(value));
+    }
+  };
   return (
     <Card className="h-full w-full mt-2 bg-transparent shadow-none">
       <>
+        <script
+          src="https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/@mediapipe/control_utils/control_utils.js"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/@mediapipe/hands/hands.js"
+          crossorigin="anonymous"
+        ></script>
         <CardHeader
           floated={false}
           shadow={false}
@@ -136,14 +354,10 @@ export default function CrearSeniaOP({ id_seccion }) {
                       <Typography variant="h3" color="black">
                         Cámara
                       </Typography>
-                      <Webcam
-                        audio={false}
-                        ref={webcamRef}
-                        screenshotFormat="image/jpeg"
-                      />
+                      <video className="input_video" ref={videoRef}></video>
                     </div>
                   </Tooltip>
-                  <Tooltip content="Camara">
+                  <Tooltip content="Procesado">
                     <div
                       key={1}
                       className="rounded-2xl  shadow-2xl cursor-pointer"
@@ -151,23 +365,33 @@ export default function CrearSeniaOP({ id_seccion }) {
                       <Typography variant="h3" color="black">
                         Procesado
                       </Typography>
-                      {/* 
-                          <Typography variant="lead" color="deep-purple">
-                            {item.video}
-                          </Typography>
-                          */}
-                      <img
-                        src={"/img/Home/pdf_bernouli.png"}
-                        alt=""
-                        className="h-auto"
+
+                      <canvas
+                        className="output_canvas border-4 border-black"
+                        ref={canvasRef}
                       />
-                      {/*
-                          <li>{item.miniatura}</li>
-                          <li>{item.link}</li>
-                           */}
                     </div>
                   </Tooltip>
                 </div>
+                <>
+                  {EnviarSocket ? (
+                    <Button
+                      className="ml-auto flex gap-1 mt-4 mx-auto bg-light-blue-900 h-11"
+                      onClick={() => CambiarEnviar(false)}
+                    >
+                      <PlusCircleIcon className="h-7 w-7" />
+                      <p className="mt-1">"Dejar de enviar"</p>
+                    </Button>
+                  ) : (
+                    <Button
+                      className="ml-auto flex gap-1 mt-4 mx-auto bg-light-blue-900 h-11"
+                      onClick={() => CambiarEnviar(true)}
+                    >
+                      <PlusCircleIcon className="h-7 w-7" />
+                      <p className="mt-1">"Enviar Imagenes"</p>
+                    </Button>
+                  )}
+                </>
               </CardBody>
             </Card>
           </Collapse>
--- src/pages/OP/index.js
+++ src/pages/OP/index.js
@@ -1,2 +1,3 @@
 
 export { default as CrearSeniaOP } from "@/pages/OP/CrearSeniaOP";
+
